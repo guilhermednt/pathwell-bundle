e43a53e68ec4136326ce50acd4961a35da681e2a
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-07-04 15:48:36 -0300

    fixed root config name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('donato_pathwell');
+        $rootNode = $treeBuilder->root('donato_path_well');
 
         $rootNode
             ->children()
